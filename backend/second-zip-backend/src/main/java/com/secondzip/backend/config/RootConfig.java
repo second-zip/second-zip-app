3e2cfcd16136c22d824eacafd30c0dda7b4d5b53
jjuny0326 <140596682+jjuny0326@users.noreply.github.com>
2026-07-20 16:42:45 +0900

    chore(BE): 백엔드 초기 세팅 (#6)

---
+++ backend/second-zip-backend/src/main/java/com/secondzip/backend/config/RootConfig.java
@@ -0,0 +1,72 @@
+package com.secondzip.backend.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.log4j.Log4j2;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Log4j2
+@MapperScan(basePackages = {"com.secondzip"})
+public class RootConfig {
+    @Value("${DB_DRIVER}") String driver;
+    @Value("${DB_URL}") String url;
+    @Value("${DB_USERNAME}") String username;
+    @Value("${DB_PASSWORD}") String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:/mybatis-config.xml"));
+        sqlSessionFactory.setDataSource(dataSource());
+
+        // ===== 이 줄이 빠져있었어요 =====
+        sqlSessionFactory.setMapperLocations(
+                applicationContext.getResources("classpath:/com/secondzip/mapper/**/*.xml")
+        );
+
+        return (SqlSessionFactory) sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(){
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+
+        // 클래스패스에서 직접 찾음 -> user.dir, VM 옵션, 빌드 폴더 깊이와 완전 무관
+        configurer.setLocation(new ClassPathResource(".env"));
+        configurer.setIgnoreResourceNotFound(true);
+        configurer.setIgnoreUnresolvablePlaceholders(false);
+
+        return configurer;
+    }
+}
