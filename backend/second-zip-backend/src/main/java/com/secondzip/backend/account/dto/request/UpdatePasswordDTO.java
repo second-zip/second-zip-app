3e05d1bb12d7d8f1b7be96e1e386972e30838d5f
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2026-07-28 11:40:51 +0900

    refactor(be): password validation 검증 추가

--- backend/second-zip-backend/src/main/java/com/secondzip/backend/account/dto/request/UpdatePasswordDTO.java
+++ backend/second-zip-backend/src/main/java/com/secondzip/backend/account/dto/request/UpdatePasswordDTO.java
@@ -6,20 +6,34 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @ApiModel(description = "비밀번호 변경 요청")
 @Getter
 @NoArgsConstructor
 public class UpdatePasswordDTO {
 
-    @ApiModelProperty(value = "현재 비밀번호", example = "1234", required = true)
+    @ApiModelProperty(value = "현재 비밀번호", example = "password1!", required = true)
+    @Size(min = 8, max = 100, message = "비밀번호는 8자 이상 16자 이하로 입력해야 합니다.")
     @NotBlank(message = "현재 비밀번호는 필수입니다.")
     private String currentPassword;
 
-    @ApiModelProperty(value = "새 비밀번호", example = "1234!", required = true)
+    @ApiModelProperty(value = "새 비밀번호", example = "password1!", required = true)
+    @Size(min = 8, max = 100, message = "비밀번호는 8자 이상 16자 이하로 입력해야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 포함하여 8자 이상이어야 합니다"
+    )
     @NotBlank(message = "새 비밀번호는 필수입니다.")
     private String newPassword;
 
-    @ApiModelProperty(value = "새 비밀번호 확인", example = "1234!", required = true)
+    @ApiModelProperty(value = "새 비밀번호 확인", example = "password1!", required = true)
+    @Size(min = 8, max = 100, message = "비밀번호는 8자 이상 16자 이하로 입력해야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 포함하여 8자 이상이어야 합니다"
+    )
     @NotBlank(message = "새 비밀번호 확인은 필수입니다.")
     private String newPasswordConfirm;
 }
--- backend/second-zip-backend/src/main/java/com/secondzip/backend/account/dto/request/WithdrawAccountDTO.java
+++ backend/second-zip-backend/src/main/java/com/secondzip/backend/account/dto/request/WithdrawAccountDTO.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBl
 @NoArgsConstructor
 public class WithdrawAccountDTO {
 
-    @ApiModelProperty(value = "회원 탈퇴 확인을 위한 현재 비밀번호", example = "password123", required = true)
+    @ApiModelProperty(value = "회원 탈퇴 확인을 위한 현재 비밀번호", example = "password1!", required = true)
     @NotBlank(message = "현재 비밀번호는 필수입니다.")
     private String password;
 }
