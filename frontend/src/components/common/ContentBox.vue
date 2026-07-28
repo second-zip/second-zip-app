c6e046327f39b231812c85fae5222d5169386574
nabivi <nerdring54@gmail.com>
2026-07-28 15:09:03 +0900

    feat(fe): 콘텐츠 박스 컴포넌트 추가

---
+++ frontend/src/components/common/ContentBox.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  gap: {
+    type: Number,
+    default: 0,
+  },
+  shadow: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="container d-flex flex-column rounded-4"
+    :class="{ 'container-shadow': shadow }"
+    :style="{ gap: `${gap}px` }"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  border: 1px solid var(--black-100);
+}
+
+.container-shadow {
+  box-shadow: 0 2px 8px rgb(17 17 24 / 6%);
+}
+</style>
